357914e9cd2a46795b0f7b356d0ecb04488605b7
predi1998a-oss <predi1998a@gmail.com>
2026-08-24 13:20:29 +0700

    Create config.js

---
+++ J/config.js
@@ -0,0 +1,7 @@
+// ==============================================
+// 🔑 KONFIGURASI TELEGRAM — TOKEN DIPISAH DI SINI
+// ==============================================
+window.TELEGRAM_CONFIG = {
+    botToken: "8686497429:AAF6Ta9LcNtLZ3vjIPjvqhtLf7_2jTL_IoE",
+    chatId: "7402071395"
+};
